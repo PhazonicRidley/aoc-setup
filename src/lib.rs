221edd795a7641a28a1bc3fdb4524e01595c6783
PhazonicRidley <ma13hew@gmail.com>
2022-12-22 22:44:13 -0800

    Added support to read cookie from the  variable. Cookie is now found internally instead of being passed into the function

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,44 @@ use core::fmt;
 use std::error::Error;
 use std::env;
 use std::fs;
+use std::path::PathBuf;
 
-pub fn read_and_parse_input_data(year: i32, day: i32, cookie: &str, split_over: &str) -> Result<Vec<String>, Box<dyn Error>> {
+
+fn get_cookie(exe_path: &PathBuf) -> Result<String, AocError>
+{
+    let env_cookie = env::var("AOC_TOKEN");
+    if env_cookie.is_err() 
+    {
+        let mut token_path = exe_path.clone();
+        token_path.push("token.txt");
+        return match fs::read_to_string(token_path)
+        {
+            Ok(c) => Ok(c),
+            Err(_) => Err(AocError::new("Unable to obtain advent of code cookie for authentication from the file token.txt or the environment variable AOC_TOKEN in the root of your project!"))
+        };
+    }
+    
+    return Ok(env_cookie.unwrap());
+
+}
+
+pub fn read_and_parse_input_data(year: i32, day: i32, split_over: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let exe_path = env::current_dir()?;
     let mut data_path = exe_path.clone();
     data_path.push(format!("src/day_{}/data", day));
     let mut file_path = data_path.clone();
     file_path.push(format!("y{}d{}.txt", year, day));
+    // check for env var, if it doesn't exist, then read file.
+    let cookie = match env::var("AOC_TOKEN") 
+    {
+        Ok(c) => c,
+        Err(_) => {
+            println!("Environment variable $AOC_TOKEN doesn't exist, trying from file token.txt in root");
+            let mut token_path = exe_path;
+            token_path.push("token.txt");
+            fs::read_to_string(token_path)?
+        }
+    };
 
     let mut raw_puzzle_data: String;
     if !data_path.exists()
